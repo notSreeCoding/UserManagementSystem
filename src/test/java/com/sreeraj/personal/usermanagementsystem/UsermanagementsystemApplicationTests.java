2bc131652df9a3d4df8659f0859e68bebe4e1096
notSreeCoding <sreerajr342@gmail.com>
2024-11-12 21:59:26 +0530

    initial

---
+++ src/test/java/com/sreeraj/personal/usermanagementsystem/UsermanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreeraj.personal.usermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
